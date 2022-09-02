2ef4b42b9b80ac0a4554c303d763f6736fce0f1d
Drake Axelrod <drake@draxel.io>
2022-09-02 18:56:52 +0200

    update

---
+++ a1/Pet.cs
@@ -0,0 +1,29 @@
+namespace A1
+{
+  class Pet
+  {
+    private string name;
+    private int age;
+    private bool isFemale;
+
+    public void Start()
+    {
+      Console.WriteLine();
+      Console.WriteLine("Greetings from the Pet Owner application!");
+      Console.WriteLine();
+
+      ReadAndSavePetaData();
+      DisplayPetInfo();
+    }
+
+    public void ReadAndSavePetaData()
+    {
+
+    }
+
+    public void DisplayPetInfo()
+    {
+
+    }
+  }
+}
--- a1/Program.cs
+++ a1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace A1 {
+  class Program
+  {
+      static void Main(string[] args)
+      {
+          Console.BackgroundColor = ConsoleColor.Gray;
+          Console.Clear();
+          Console.ForegroundColor = ConsoleColor.Black;
+          Console.Title = "Pet Owner";
+
+          // Console.WriteLine("Hello World!");
+          Pet petObj = new Pet();
+          petObj.Start();
+          
+          Console.WriteLine("Press enter to start next object!");
+          Console.ReadLine();
+      }
+  }
+}
