986b589da8be97669d7b059fc453a175f4f7983a
Drake <drake@draxel.io>
2022-10-02 21:13:52 +0200

    Update

---
+++ a2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class MainProgram
+{
+  public static void Main(String [] args)
+  {
+    Console.Title = "Strings, selection and iterations in C#";
+    FunFeatures ff = new FunFeatures();
+    ff.Start();
+    
+    ContinueToNextPart();
+  }
+
+  private static void ContinueToNextPart()
+  {
+    Console.WriteLine("\nPRESS ENTER TO CONTINUE TO THE NEXT PART");
+    Console.ReadLine();
+    Console.Clear();
+  }
+}
